f8c22b8b18ef631374f01067ea747bf1f7d98e59
Chelsea Farley <chelseafarley91@gmail.com>
2021-12-24 09:07:55 +1300

    Clipboard Demo for Expo React Native Apps

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, TextInput, View } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
+import React from 'react';
 
 export default function App() {
+  const [inputValue, setInputValue] = React.useState("");
+
+  const bankAccount = "01-545455444-4343-00";
+  const copyText = (text) => {
+    Clipboard.setString(text);
+  };
+
+  const setInputToCopiedText = async () => {
+    const text = await Clipboard.getStringAsync();
+    setInputValue(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{bankAccount}</Text>
+      <Button title="Copy Bank Account" onPress={() => copyText(bankAccount)} />
+      <TextInput onChangeText={setInputValue} value={inputValue} />
+      <Button title="Set Input Value" onPress={setInputToCopiedText} />
       <StatusBar style="auto" />
     </View>
   );
